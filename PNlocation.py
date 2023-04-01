d294b819a1728b96cf5bbcf54f2cd52a824b576f
Beatriz Simões <121191936+beatrizlsimoes@users.noreply.github.com>
2023-04-01 10:27:12 +0100

    Delete PNlocation.py

--- PNlocation.py
+++
@@ -1,7 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder 
-phone_number1 = phonenumbers.parse("+351000000000")
-
-print("\nPhone Numbers location\n")
-print(geocoder.description_for_number(phone_number1,"en"))
-
